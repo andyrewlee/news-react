488f6a0122743ea76a84ea864a361a1605ed2829
Andrew Lee <andrew.lee@hired.com>
2019-08-12 20:50:06 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=61dbc3b8b96c46868cb2ff68f404483d';
+
+const Article = (props) => {
+  return (
+    <div>
+      <h2>{props.author}</h2>
+      <h2>{props.content}</h2>
+    </div>
+  );
+};
+
+const ArticleCounter = (props) => {
+  return (
+    <h1>{props.articleCount} articles</h1>
+  )
+}
+
+const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  fetch(API_URL)
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      setArticles(res.articles);
+    });
+
+  const renderArticles = () => {
+    const renderedArticles = [];
+    for (let i = 0; i < articles.length; i += 1) {
+      const currentArticle = articles[i];
+      renderedArticles.push(
+        <Article
+          key={currentArticle.id}
+          author={currentArticle.author}
+          content={currentArticle.content}
+        />
+      );
+    }
+
+    if (articles.length === 0) {
+      return 'Loading...';
+    }
+
+    return renderedArticles;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ArticleCounter articleCount={articles.length}  />
+      {renderArticles()}
     </div>
   );
 }
